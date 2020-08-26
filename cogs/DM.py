75d50e503d73225af25ccb06930af6083e2dbcd2
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2020-08-26 16:45:41 +0200

    Update DM.py

--- cogs/DM.py
+++ cogs/DM.py
@@ -7,7 +7,7 @@ class DM(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.BOT_GUILD_FOR_DM = 714802740345176074
-        self.CATEGORY_NAME_FOR_DM = 'DMs TO CIRO'
+        self.CATEGORY_NAME_FOR_DM = f'DMs TO {self.bot.user}'
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -20,21 +20,27 @@ class DM(commands.Cog):
                     }
                     category = discord.utils.get(self.bot.get_guild(self.BOT_GUILD_FOR_DM).categories, name=self.CATEGORY_NAME_FOR_DM)
                     if str(category) == 'None':
-                        await self.bot.get_guild(self.BOT_GUILD_FOR_DM).create_category(self.CATEGORY_NAME_FOR_DM)
+                        overwrites = {
+                            self.bot.get_guild(self.BOT_GUILD_FOR_DM).default_role: discord.PermissionOverwrite(
+                                read_messages=False),
+                            self.bot.get_guild(self.BOT_GUILD_FOR_DM).me: discord.PermissionOverwrite(
+                                read_messages=True)
+                        }
+                        await self.bot.get_guild(self.BOT_GUILD_FOR_DM).create_category(self.CATEGORY_NAME_FOR_DM, overwrites=overwrites)
 
                     category = discord.utils.get(self.bot.get_guild(self.BOT_GUILD_FOR_DM).categories, name=self.CATEGORY_NAME_FOR_DM)
 
                     channel = await self.bot.get_guild(self.BOT_GUILD_FOR_DM).create_text_channel(f'{message.author.id}', overwrites=overwrites, category=category, topic=f'{message.author.name}')
                     member = message.author
-                    await channel.send(message.content)
+                    await channel.send(f"> `ok{message.author}`\n{message.content}")
                 elif discord.utils.get(self.bot.get_guild(self.BOT_GUILD_FOR_DM).channels, name=f'{message.author.id}'):
                     channel = discord.utils.get(self.bot.get_guild(self.BOT_GUILD_FOR_DM).channels, name=f'{message.author.id}')
-                    await channel.send(message.content)
+                    await channel.send(f"> `{message.author}`\n{message.content}")
             elif str(message.channel.category) == self.CATEGORY_NAME_FOR_DM:
                 try:
                     channel = self.bot.get_user(int(message.channel.name))
-                    await channel.send(message.content)
-                except discord.Forbidden:
+                    await channel.send(f"> `{message.author}`\n{message.content}")
+                except discord.Forbidden or AttributeError:
                     await message.channel.send('`Non ho più contatti con questo utente` O `L\' utente blocca i messaggi privati`')
 
 
